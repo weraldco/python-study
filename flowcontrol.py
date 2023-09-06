2abc91d849a00eb59428503350af02fe4759db11
Werald Opolento <werald.opolento@gmail.com>
2023-09-06 09:07:05 +0800

    Day 2 of python

---
+++ flowcontrol.py
@@ -0,0 +1,52 @@
+## if statement
+
+# name = input("Whats your name? ")
+# age = int(input("How old are you? "))
+# # AND
+# if age >= 1 and age < 18:
+#     print("You're a minor")
+# else:
+#     print("You're not a minor anymore!")
+
+
+
+## elif
+
+
+# print("Enter your average  grade this sem: ")
+# grade = int(input())
+
+# if grade >= 75 and grade <= 80:
+#     print("Average!")
+# elif grade >= 81 and grade <= 85:
+#     print("Excellent!")
+# elif grade >= 86 and grade <= 90:
+#     print("Magnificent!")
+# elif grade >= 91 and grade <= 100:
+#     print("Great job, you did so well!")
+# else:
+#     print("You have failed this semester..")
+
+
+
+## While loop
+
+
+# while True: 
+#     print("Enter you username: ")
+#     username = input()
+#     if username != "werald":
+#         continue
+#     while True:
+#         print("Hello Werald, can you enter your password? ")
+#         password = input()
+#         if password == "wewe":
+#             break
+#     break
+
+# print("Access granted!")
+
+
+## For loop
+
+
---
+++ test.py
@@ -0,0 +1,26 @@
+
+while True:
+    print("Please enter your name.")
+    name = input()
+    if name == 'your name':
+        print(name)
+        break
+    
+print("Thank you")
+
+
+
+
+# name = "Alice" * 5
+
+# print(str(name))
+
+
+# # Float
+
+# total = 7.7 + 1
+
+# print(int(total))
+
+# print(round(25.5))
+
